c63bfc119ea6cca970a40b5f19e6104d3416ddf2
mykenzie <mykenzierogers@gmail.com>
2019-09-25 17:33:55 -0500

    initial commit

--- server/index.js
+++ server/index.js
@@ -4,21 +4,39 @@ const server = require("http").Server(app);
 const sockets = require("socket.io")
 const io = sockets(server);
 const session = require("express-session");
+const massive = require("massive");
 
 let messages = [];
+let db = null;
 
-app.post("/login", (req, res) => {
-    req.session.username = req.body.username
+massive(`postgres://wssojhwftczjth:318e62586d5fea3946dd34eb44acfc1c4931f1acd997e198cb3c96e76b9c1e83@ec2-174-129-227-128.compute-1.amazonaws.com:5432/dchrtmlau4ctnd?ssl=true`).then(dbInstance => {
+    db = dbInstance;
 })
 
 io.on("connection", socket => {
+
     socket.emit("onConnection", {
         message: "Sockets has been connected"
     })
 
     socket.on("messageSend", data => {
-        messages.push(data.message);
-        io.emit("newMessage", {messages})
+        console.log(db);
+        //rooms table
+        //messages table
+        const {username, message, roomName} = data;
+        let roomIndex = messages.findIndex(val => val.roomName === roomName); //check to see if the room exists
+        if(roomIndex === -1) {
+            messages.push({
+                roomName,
+                messages: []
+            }) //create a room
+            roomIndex = messages.length - 1;
+        } 
+        messages[roomIndex].messages.push(message); //insert into messages table with room id
+        io.emit("newMessage", {
+            messages: messages[roomIndex].messages //send back messages with that room id
+        })
+        console.log(messages);
     })
 })
 
--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import io from "socket.io-client";
-const socket = io("http://localhost");
 
-function App() {
+function App(props) {
+  console.log(props.match.params.roomName);
   const [messages, setMessages] = React.useState([]);
   const [userMessage, setUserMessage] = React.useState("");
+  const [socket, setSocket] = React.useState(null);
+  React.useEffect(() => {
+    console.log("mounted");
+    setSocket(io("http://localhost"))
+  }, [])
 
-  socket.on("newMessage", data => console.log("new messages") || setMessages(data.messages))
+  if(socket) {
+    socket.on("newMessage", data => console.log("new messages") || setMessages(data.messages))
+  }
 
   return (
     <div className="App">
@@ -19,7 +26,11 @@ function App() {
       onChange={e => setUserMessage(e.target.value)} />
       <button
       onClick={() => {
-        socket.emit("messageSend", {message: userMessage})
+        socket.emit("messageSend", {
+          username: "test",
+          message: userMessage,
+          roomName: props.match.params.roomName
+        })
       }}>
         Send Message
       </button>
